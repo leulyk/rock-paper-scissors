4b28d2fab064b9805f8b34e4a5a215fa0cb143d5
Leul Yiheyis <leulykpro@gmail.com>
2022-03-19 14:37:10 +0300

    add the play function

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function computerPlay () {
   const options = ['Rock', 'Paper', 'Scissors'];
   return options[Math.floor(Math.random() * options.length)];
 }
+
+function play (playerSelection, computerSelection) {
+  const pSelection = playerSelection.toLowerCase();
+  const cSelection = computerSelection.toLowerCase();
+  const isWin = ((pSelection === 'paper') && (cSelection === 'rock')) ||
+                ((pSelection === 'rock') && (cSelection === 'scissors')) ||
+                ((pSelection === 'scissors') && (cSelection === 'paper'));
+
+  if (isWin) {
+    return `You Win!!! ${pSelection} beats ${cSelection}.`;
+  } else {
+    if (pSelection === cSelection) {
+      return 'Draw!!!';
+    }
+    return `You Lose!!! ${cSelection} beats ${pSelection}.`;
+  }
+}
