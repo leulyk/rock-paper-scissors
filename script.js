706384605770178f33e5bc7964eee3c975765e61
Leul Yiheyis <leulykpro@gmail.com>
2022-03-19 15:00:15 +0300

    Add the game method Handle invalid input from user Rename play function to playRound

--- script.js
+++ script.js
@@ -5,19 +5,33 @@ function computerPlay () {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function play (playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection) {
   const pSelection = playerSelection.toLowerCase();
   const cSelection = computerSelection.toLowerCase();
-  const isWin = ((pSelection === 'paper') && (cSelection === 'rock')) ||
-                ((pSelection === 'rock') && (cSelection === 'scissors')) ||
-                ((pSelection === 'scissors') && (cSelection === 'paper'));
 
-  if (isWin) {
-    return `You Win!!! ${pSelection} beats ${cSelection}.`;
-  } else {
-    if (pSelection === cSelection) {
-      return 'Draw!!!';
+  if (['rock', 'paper', 'scissors'].includes(pSelection)) {
+    const isWin = ((pSelection === 'paper') && (cSelection === 'rock')) ||
+                  ((pSelection === 'rock') && (cSelection === 'scissors')) ||
+                  ((pSelection === 'scissors') && (cSelection === 'paper'));
+
+    if (isWin) {
+      return `You Win!!! ${pSelection} beats ${cSelection}.`;
+    } else {
+      if (pSelection === cSelection) {
+        return 'Draw!!!';
+      }
+      return `You Lose!!! ${cSelection} beats ${pSelection}.`;
     }
-    return `You Lose!!! ${cSelection} beats ${pSelection}.`;
+  } else {
+    return 'Invalid input!!!';
   }
 }
+
+function game () {
+  for (let i = 1; i <= 5; ++i) {
+    console.log(playRound(window.prompt('Enter rock, paper, or scissors: '),
+      computerPlay()));
+  }
+}
+
+game();
